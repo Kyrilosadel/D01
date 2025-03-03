37621c91cf9063ec3a2597cff02f4198e755f82e
Gamaladeen <Gamaladeen@DESKTOP-BF3HP0F>
2025-03-03 20:34:21 +0200

    Add project files.

---
+++ firstProject/Program.cs
@@ -0,0 +1,16 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace firstProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           decimal X = 10;
+            decimal Y = 20;
+            Console.WriteLine($"Equation: {X} + {Y} = {X + Y:f5}");
+
+
+        }
+    }
+}
---
+++ task_1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Estimate for carpet cleaning service");
+            Console.Write("Number of small rooms:");
+            int smallRooms = Convert.ToInt32( Console.ReadLine());
+            Console.Write("Number of large rooms:");
+            int largeRooms = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Price per small room: $25");
+            Console.WriteLine("Price per large room: $35");
+            int cost = (smallRooms * 25) + (largeRooms * 35);
+            double taxs = cost * 0.06;
+            Console.WriteLine($"Cost : ${cost}");
+            Console.WriteLine($"Tax: ${taxs}");
+            Console.WriteLine("-_______________-");
+
+            double total = cost + taxs;
+            Console.WriteLine($"Total estimate: ${total}");
+            Console.WriteLine("This estimate is valid for 30 days");
+
+        }
+    }
+}
